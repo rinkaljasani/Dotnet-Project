c01f0c1e80ecced247a48b8378296e1a3d6d2e5b
rinkal <rinkaljasani95@gmail.com>
2020-01-21 13:52:34 -0500

    Initial Commmit

---
+++ practical 1/Dotnet/pattern1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dotnet
+{
+    class pattern1
+    {
+        public void main(String s[])
+        {
+        for(int i=0;i<=5;i++)
+        {
+            for(int j=0;j<=5;j++)
+            {
+                Console.writeln("*");
+            }
+        }
+        }
+    }
+}
---
+++ practical 2/practical 2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practical_2
+{
+    class Program
+    {
+        static void Rectangle()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(" * ");
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+          
+        }
+        static void Tringle_left()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(" ");
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" * ");
+                }
+            }
+            
+        }
+        static void Tringle_right()
+        {
+
+            for (int i = 5; i > 0; i++)
+            {
+                Console.WriteLine(" ");
+                for (int j = i; j < 5; j++)
+                {
+                    Console.Write(" * ");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Rectangle();
+            Console.WriteLine();
+            Tringle_left();
+            Console.WriteLine();
+            Tringle_right();
+            Console.Read();
+        }
+    }
+}
